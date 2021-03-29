b0ac27c1847e2f9e56595c8d4b67b56ec7b8a3cb
Rana Zaman <81526546+rana-zaman@users.noreply.github.com>
2021-03-29 12:41:11 +0500

    Create Zaman.py
    $ pkg update
    $ pkg upgrade
    $ pkg install python
    $ pkg install python2
    $ pip2 install requests
    $ pip2 install mechanize
    $ pip2 install bs4
    $ pkg install git
    $ git clone https://github.com/rana-zaman/RANAZAMAN.git
    $ cd zaman
    $ python2 zaman.py

---
+++ Zaman.py
@@ -0,0 +1,79 @@
+-  @@ -1 +1,42 @@
++ # Rajput
++ # Rajput
+
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Update README.md
+
+ main
+@rana-zaman
+rana-zaman committed on march 29
+1 parent 80b65e1 commit 571543b6dbef3ca8d2d4299d398a46a52fd80a45
+Showing  with 42 additions and 1 deletion.
+  43  README.md 
+@@ -1 +1,42 @@
+# Rajput
+# Rajput
+<b></b> </br> <br>[![Github](https://img.shields.io/badge/Github-rana-zaman-dimgray?style=flat-square&logo=github)](https://github.com/rana-zaman/)<br> [![Facebook](https://img.shields.io/badge/Facebook-+ZAMAN-blue?style=flat-square&logo=facebook)](https://www.facebook.com/Apni.bapka.account7)<br> [![Instagram](https://img.shields.io/badge/Instagram-JAMES404-hotpink?style=flat-square&logo=instagram)](I'm on Instagram as @jame404404. Install the app to follow my photos and videos. https://www.instagram.com/invites/contact/?i=1xbkroj44l0gh&utm_content=2gx3kse)<br> [![Whatsapp](https://img.shields.io/badge/Whatsapp-James-deepgreen?style=flat-square&logo=whatsapp)](https://chat.whatsapp.com/Dy3uWB9hOsrCvu49DaKP1n)
+
+
+
+<h1 align="center"> |MR.ZAMAN|</h1>
+
+<h2 align="center"> FB CRACKING TOOL WITHOUT LOGIN </h2>
+
+<p align="center">
+      PAKISTANI ALL SIM FB CRACK
+</p>
+
+
+
+
+
+
+## <b>installation</b>
+
+```
+$ pkg update
+$ pkg upgrade
+$ pkg install python
+$ pkg install python2
+$ pip2 install requests
+$ pip2 install mechanize
+$ pip2 install bs4
+$ pkg install git
+$ git clone https://github.com/rana-zaman/RANAZAMAN.git
+$ cd zaman
+$ python2 zaman.py
+```
+
+# Single Command 
+
+```
+pkg update ; pkg upgrade ; pkg install python ; pkg install python2 ; pip2 install requests ; pip2 install mechanize ; pip2 install bs4 ; pkg install git ; git clone https://github.com/rana-zaman/RANAZAMAN.git ; cd Paknumber ; python2 zaman.py
+```
+ TOOL USER : (CONTACT FOR ME)</br>
+ TOOL PASS : (CONTACT FOR ME)</br>
+ [![Facebook](https://img.shields.io/badge/Facebook-zaman-blue?style=flat-square&logo=facebook)](https://www.facebook.com/Apni.bapka.account7)</br>
+0 comments on commit 571543b
+
+Leave a comment
+ You’re not receiving notifications from this thread.
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
